ca8867c38547821f5e289b4585e7113cee2404b2
Razy <97766343+razyness@users.noreply.github.com>
2022-10-01 00:06:28 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,39 +1,30 @@
-import discord
 import os
 import asyncio
-import logging
 from discord.ext import commands
+from sakana import *
 
-prefix = '.'
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
 intents.message_content = True
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(
-    filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-
 ce = commands.Bot(command_prefix=prefix, intents=intents)
 
-
-@ce.event
-async def on_message(message):
-    if 'oh' in message.content and message.author.bot is False:
-        await message.channel.send('oh')
-    await ce.process_commands(message)
 
 async def load():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
+            print(filename + ' was loaded')
             await ce.load_extension(f'cogs.{filename[:-3]}')
+        else:
+            print(filename + ' was not loaded')
     await ce.load_extension('jishaku')
+    print('all extensions loaded')
 
 
 asyncio.run(load())
-ce.run(token)
+ce.run(TOKEN, log_handler=None)
