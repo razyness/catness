cf5ea7499f1b1210daa8be48ebf36c4a6cf35714
Razy <97766343+razyness@users.noreply.github.com>
2022-10-01 00:06:53 +0200

    Add files via upload

---
+++ sakana.py
@@ -0,0 +1,23 @@
+# hello
+
+import logging.handlers
+import discord
+
+TOKEN = 'OTQzOTQyMzU2MTQxNDgyMDc0.GUmEbN.tepFChtGTRHRByHzWR7Ey0Upm2XjGRTpugfJQs'
+
+prefix = '.'
+
+logger = logging.getLogger('discord')
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+intents = discord.Intents.default()
