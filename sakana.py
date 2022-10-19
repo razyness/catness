72e4d6242638820a696db44c8d0196a1a8895b3c
razyness <razydiscord@protonmail.com>
2022-10-19 18:24:11 +0200

    deleted:    cogs/__pycache__/youtube apk.cpython-310.pyc

--- sakana.py
+++ sakana.py
@@ -3,21 +3,21 @@
 import logging.handlers
 import discord
 
-TOKEN = 'the token'
+TOKEN = 'MTAyOTg2NDYyMTA0NzMwNDIwMw.GDgMi-.G-O0LjHPuJqydXEs7ZYTLc_OveWNiVGnmK95Tg'
 
 prefix = '.'
 
-logger = logging.getLogger('discord')
-
-handler = logging.handlers.RotatingFileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    maxBytes=32 * 1024 * 1024,  # 32 MiB
-    backupCount=5,  # Rotate through 5 files
-)
-dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# logger = logging.getLogger('discord')
+# 
+# handler = logging.handlers.RotatingFileHandler(
+#     filename='discord.log',
+#     encoding='utf-8',
+#     maxBytes=32 * 1024 * 1024,  # 32 MiB
+#     backupCount=5,  # Rotate through 5 files
+# )
+# dt_fmt = '%Y-%m-%d %H:%M:%S'
+# formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 intents = discord.Intents.default()
