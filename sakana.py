18d5fe0cc83929d1dd5633596ac1b112121f6919
razyness <razydiscord@protonmail.com>
2022-12-11 15:15:12 +0100

    hi

--- sakana.py
+++ sakana.py
@@ -5,20 +5,8 @@ import discord
 
 TOKEN = "the token in question"
 LASTFM_API_KEY = "the api key goes here"
+STEAM_API_KEY = "read above"
 
 prefix = '.'
-
-# logger = logging.getLogger('discord')
-# 
-# handler = logging.handlers.RotatingFileHandler(
-#     filename='discord.log',
-#     encoding='utf-8',
-#     maxBytes=32 * 1024 * 1024,  # 32 MiB
-#     backupCount=5,  # Rotate through 5 files
-# )
-# dt_fmt = '%Y-%m-%d %H:%M:%S'
-# formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
 
 intents = discord.Intents.default()
