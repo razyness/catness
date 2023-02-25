b53a3a47d829d43958186deeb09b3509f03590e7
razyness <razydiscord@protonmail.com>
2023-02-25 16:02:28 +0100

    new file:   cogs/Fun/reaction.py

---
+++ cogs/Fun/reaction.py
@@ -0,0 +1,25 @@
+import toml
+import aiohttp
+from discord.ext import commands
+from discord import app_commands
+
+config = toml.load("config.toml")
+TENOR = config["TENOR"]
+
+class Reaction(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+
+	@app_commands.command(name='reaction', description='Live slug reaction')
+	async def reaction(self, interaction):
+		try:
+			async with aiohttp.ClientSession() as session:
+				async with session.get(f"https://tenor.googleapis.com/v2/search?q=live-reaction&key={TENOR}&client_key=tenor-api&limit=50&random=true") as response:
+					result = await response.json()
+					result = result["results"][0]["itemurl"]
+			await interaction.response.send_message(result)
+		except Exception as e:
+			print(e)
+	
+async def setup(client):
+	await client.add_cog(Reaction(client))
