b3488f507c1c9bfc4471898e67101ecccc175c0a
razyness <razydiscord@protonmail.com>
2023-06-19 20:22:18 +0200

    it didn't like dicts

--- utils/cache.py
+++ utils/cache.py
@@ -1,11 +1,18 @@
 import functools
 import asyncio
 from datetime import datetime, timedelta
+from collections import OrderedDict
 
 
-def cache(expiration_time=None, invalidation_interval=None):
+class CacheEntry:
+    def __init__(self, result, expiration_time):
+        self.result = result
+        self.expiration_time = expiration_time
+
+
+def cache(expiration_time=None, invalidation_interval=None, max_size=None):
     """
-    A general-purpose caching decorator for asynchronous functions.
+    An improved caching decorator for asynchronous functions.
 
     Args:
         expiration_time (int or timedelta): Optional. The time in seconds or
@@ -16,30 +23,39 @@ def cache(expiration_time=None, invalidation_interval=None):
             or timedelta object at which the cache should be invalidated and updated.
             If not provided, the cache will not be automatically invalidated.
 
+        max_size (int): Optional. The maximum number of entries to store in the cache.
+            If the cache exceeds this size, the least recently used item will be evicted.
+
     Returns:
         coroutine function: The decorated asynchronous function.
     """
-    memo = {}
+    cache_dict = OrderedDict()
 
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
-            key = (func.__name__, args, frozenset(kwargs.items()))
+            key = (func.__name__, _hashable_args(args), _hashable_kwargs(kwargs))
 
-            if key in memo:
-                if expiration_time is None or memo[key][0] > datetime.now():
-                    return memo[key][1]
+            if key in cache_dict:
+                cache_entry = cache_dict[key]
+                if expiration_time is None or cache_entry.expiration_time > datetime.now():
+                    return cache_entry.result
                 else:
-                    del memo[key]
+                    del cache_dict[key]
 
             result = await func(*args, **kwargs)
-            memo[key] = (datetime.now() + _get_timedelta(expiration_time), result)
+            expiration = _get_expiration_time(expiration_time)
+            cache_dict[key] = CacheEntry(result, expiration)
+
+            if max_size is not None and len(cache_dict) > max_size:
+                cache_dict.popitem(last=False)
+
             return result
 
         async def cache_invalidation_task():
             while True:
                 await asyncio.sleep(_get_seconds(invalidation_interval))
-                memo.clear()
+                cache_dict.clear()
 
         if invalidation_interval is not None:
             asyncio.ensure_future(cache_invalidation_task())
@@ -48,18 +64,36 @@ def cache(expiration_time=None, invalidation_interval=None):
 
     return decorator
 
-def _get_timedelta(duration):
+
+def _hashable_args(args):
+    return tuple(_hashable(item) for item in args)
+
+
+def _hashable_kwargs(kwargs):
+    return frozenset((key, _hashable(value)) for key, value in kwargs.items())
+
+
+def _hashable(item):
+    if isinstance(item, dict):
+        return _hashable_kwargs(item)
+    return item
+
+
+def _get_expiration_time(duration):
+    if duration is None:
+        return None
     if isinstance(duration, timedelta):
-        return duration
-    elif isinstance(duration, int):
-        return timedelta(seconds=duration)
-    else:
-        raise ValueError("Invalid duration format. Please provide an int or timedelta.")
+        return datetime.now() + duration
+    if isinstance(duration, int):
+        return datetime.now() + timedelta(seconds=duration)
+    raise ValueError("Invalid duration format. Please provide an int or timedelta.")
+
 
 def _get_seconds(duration):
+    if duration is None:
+        return None
     if isinstance(duration, timedelta):
         return duration.total_seconds()
-    elif isinstance(duration, int):
+    if isinstance(duration, int):
         return duration
-    else:
-        raise ValueError("Invalid duration format. Please provide an int or timedelta.")
+    raise ValueError("Invalid duration format. Please provide an int or timedelta.")
