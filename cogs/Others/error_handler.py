cf605060951c154d138153e203853d36c53ea18e
razyness <razydiscord@protonmail.com>
2023-06-19 20:21:58 +0200

    i forgot

--- cogs/Others/error_handler.py
+++ cogs/Others/error_handler.py
@@ -6,7 +6,7 @@ import time
 class errorHandler(commands.Cog):
     def __init__(self, ce):
         self.ce = ce
-        #self.ce.tree.on_error = self.on_tree_error
+        self.ce.tree.on_error = self.on_tree_error
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -19,22 +19,22 @@ class errorHandler(commands.Cog):
         elif isinstance(error, commands.BadArgument):
             await ctx.send(error)
 
-    #async def on_tree_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
-    #    if isinstance(error, app_commands.CommandOnCooldown):
-    #        retry_after = error.retry_after + time.time()
-    #        message = f"Command on cooldown! Try again <t:{int(retry_after)}:R>"
-#
-    #    elif isinstance(error, app_commands.MissingPermissions):
-    #        message = f"You're missing permissions to use that"
-    #    elif isinstance(error, app_commands.AppCommandError):
-    #        message = f"oopsie detected - `{error}"
-    #    else:
-    #        raise Exception
-#
-    #    try:
-    #        return await interaction.response.send_message(message, ephemeral=True)
-    #    except:
-    #        return await interaction.followup.send(message, ephemeral=True)
+    async def on_tree_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.CommandOnCooldown):
+            retry_after = error.retry_after + time.time()
+            message = f"Command on cooldown! Try again <t:{int(retry_after)}:R>"
+
+        elif isinstance(error, app_commands.MissingPermissions):
+            message = f"You're missing permissions to use that"
+        elif isinstance(error, app_commands.AppCommandError):
+            message = f"oopsie detected - `{error}"
+        else:
+            raise Exception
+
+        try:
+            return await interaction.response.send_message(message, ephemeral=True)
+        except:
+            return await interaction.followup.send(message, ephemeral=True)
 
 async def setup(ce):
     await ce.add_cog(errorHandler(ce))
