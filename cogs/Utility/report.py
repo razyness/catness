6b9190bcf3648666815350efd009aa186af46f3f
razyness <razydiscord@protonmail.com>
2023-10-27 19:45:29 +0200

    webhook created on report send

--- cogs/Utility/report.py
+++ cogs/Utility/report.py
@@ -1,15 +1,14 @@
 import discord
 import datetime
-import aiohttp
 
 from discord.ext import commands
 from discord import app_commands
 
 
 class ConfirmModal(discord.ui.Modal):
-    def __init__(self, *, title: str = "Big bug report modal cool", timeout: float | None = 120, ce) -> None:
+    def __init__(self, *, title: str = "Big bug report modal cool", timeout: float | None = 120, bot) -> None:
         super().__init__(title=title, timeout=timeout)
-        self.ce = ce
+        self.bot = bot
 
     short = discord.ui.TextInput(
         style=discord.TextStyle.short,
@@ -23,6 +22,7 @@ class ConfirmModal(discord.ui.Modal):
         style=discord.TextStyle.long,
         label="Details / how to reproduce",
         required=True,
+        min_length=200,
         max_length=4000,
         placeholder="When i press x button after y button it fails and like\nFeel free to use codeblocks if needed"
     )
@@ -32,32 +32,34 @@ class ConfirmModal(discord.ui.Modal):
                               description=self.long.value)
         embed.set_author(name="bug report")
         embed.set_footer(
-            text=f"submitted by {str(interaction.user)} | {interaction.user.id}",
+            text=f"submitted by @{str(interaction.user)} | {interaction.user.id}",
             icon_url=interaction.user.avatar.url)
         embed.timestamp = datetime.datetime.utcnow()
 
-        await interaction.response.send_message(f"Thank you!! While you wait, you can join support server real and maybe you can help me figure it out\nhttps://discord.gg/invitelink", ephemeral=True)
+        channel = self.bot.config['report_channel']
+        channel = self.bot.get_channel(channel) or await self.bot.fetch_channel(channel)
 
-        webhook_url = "https://canary.discord.com/api/webhooks/1109946330752745502/6PkPFZ-cgD0SXeaf_3zGf-_RLuVs-bWdn4nzCAYD8u0qTLbO3lFUS1RR_J8rLDz1MfU6"
+        webhook = await channel.create_webhook(name='bug report')
 
-        async with aiohttp.ClientSession() as session:
-            webhook = discord.Webhook.from_url(
-                url=webhook_url, session=session)
-            await webhook.send(embed=embed)
+        await webhook.send(embed=embed)
+        await webhook.delete(reason='Sent a bug report')
+
+        await interaction.response.send_message(f"Thank you!! While you wait, you can join support server real and maybe you can help me figure it out if you want\nhttps://discord.gg/invitelink", ephemeral=True)
 
 
 class Report(commands.Cog):
     """This bot is always being worked on!!
     """
-    def __init__(self, ce: commands.Bot):
-        self.ce = ce
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
     @app_commands.command(name='report', description='Report silly issues and errors please')
     @app_commands.checks.cooldown(1, 86400, key=lambda i: (i.user.id))
     async def report(self, interaction):
-        modal = ConfirmModal(ce=self.ce)
+        modal = ConfirmModal(bot=self.bot)
         await interaction.response.send_modal(modal)
 
 
-async def setup(ce: commands.Bot):
-    await ce.add_cog(Report(ce))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Report(bot))
