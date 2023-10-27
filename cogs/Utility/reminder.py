60753718a1aa4184cf9de630239101e3194ea786
razyness <razydiscord@protonmail.com>
2023-10-27 17:04:43 +0200

    reminders are now real

--- cogs/Utility/reminder.py
+++ cogs/Utility/reminder.py
@@ -1,86 +1,126 @@
 from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands, tasks
-from datetime import datetime, timedelta
+
+import asyncio
+import time as tm
+import uuid
+import discord
 
 
 class RemindObject:
-    def __init__(self, id: str, text: str, time: int, unit: str, channel: tuple = None):
+    def __init__(self, id: str, task: str, remind_time: int, channel: tuple = None, reminder_id: str = None):
         self.id = id
-        self.text = text
-        self.time = time
-        self.unit = unit
+        self.task = task
+        self.remind_time = remind_time
         self.channel = channel
+        self.reminder_id = reminder_id or str(uuid.uuid4())
 
 
 class Reminder(commands.Cog):
     """
-    Commands to set reminders. Work is not in progress because i am lazy
+    Commands to set, view and edit (soon) your reminders. Up to 5 at a time.
     """
-    def __init__(self, ce: commands.Bot):
+
+    def __init__(self, bot: commands.Bot):
         super().__init__()
-        self.ce = ce
+        self.bot = bot
         self.reminders = {}
         self.check_reminders.start()
 
     def cog_unload(self):
         self.check_reminders.cancel()
 
-    @tasks.loop(seconds=10)
+    async def send_reminder(self, remind_obj, remaining_time: int):
+        if remaining_time > 0:
+            await asyncio.sleep(remaining_time)
+
+        user = await self.bot.get_or_fetch_user(remind_obj.id)
+        if remind_obj.channel[0]:
+            if user:
+                await user.send(f"Reminder: {remind_obj.task}")
+        else:
+            channel = self.bot.get_channel(remind_obj.channel[1]) or await self.bot.fetch_channel(remind_obj.channel[1])
+            if channel:
+                await channel.send(f"{user.mention} reminder: {remind_obj.task}")
+
+        async with self.bot.db_pool.acquire() as conn:
+            await conn.execute("DELETE FROM reminders WHERE reminder_id = $1", remind_obj.reminder_id)
+
+    @tasks.loop(seconds=60)
     async def check_reminders(self):
-        current_time = datetime.now()
-        expired_reminders = []
-
-        for reminder_id, reminder in self.reminders.items():
-            reminder_time = datetime.fromtimestamp(reminder.time)
-            if reminder_time <= current_time:
-                expired_reminders.append(reminder_id)
-
-        for reminder_id in expired_reminders:
-            reminder = self.reminders.pop(reminder_id)
-            if reminder.channel[0] is False:
-                user = self.bot.get_user(int(reminder.channel[1]))
-                if user:
-                    await user.send(f"Reminder: {reminder.text}")
-            else:
-                channel = self.bot.get_channel(int(reminder.channel[0]))
-                if channel:
-                    await channel.send(f"Reminder: {reminder.text}")
+        async with self.bot.db_pool.acquire() as conn:
+            reminders = await conn.fetch("SELECT * FROM reminders WHERE remind_time <= $1", int(tm.time() + 60))
+            for reminder in reminders:
+                remind_obj = RemindObject(
+                    id=reminder['id'],
+                    task=reminder['task'],
+                    remind_time=reminder['remind_time'],
+                    channel=(reminder['private'], reminder['channel']),
+                    reminder_id=reminder['reminder_id']
+                )
+                await self.send_reminder(remind_obj, remaining_time=reminder['remind_time'] - int(tm.time()))
 
     @check_reminders.before_loop
     async def before_check_reminders(self):
-        await self.ce.wait_until_ready()
+        await self.bot.wait_until_ready()
 
     group = app_commands.Group(
-		name="reminder", description="Reminder things")
-    
+        name="reminder", description="Reminder things")
+
     @group.command(name="add", description="Set a reminder")
     @app_commands.choices(unit=[
-                   Choice(name="Day(s)", value="d"),
-                   Choice(name="Hour(s)", value="h"),
-                   Choice(name="Minute(s)", value="m")
-                   ])
+        Choice(name="Day(s)", value="d"),
+        Choice(name="Hour(s)", value="h"),
+        Choice(name="Minute(s)", value="m")
+    ])
     async def remind(self, inter: commands.Context, task: str, time: int, unit: str = "h", private: bool = False):
-        units = {"d": "days", "h": "hours", "m": "minutes"}
+        # i think this is right
+        units = {"d": 86400, "h": 3600, "m": 60}
 
-        remind_time = datetime.now() + timedelta(**{units[unit]: time})
-        remind_obj = RemindObject(str(inter.user.id), task, remind_time.timestamp(), unit, (str(inter.channel.id)) if private else (False, inter.user.id))
+        if inter.guild is None:
+            private = True
+
+        reminder_uuid = str(uuid.uuid4())
+        remind_time = int(tm.time()) + time * units[unit]
+
+        remind_obj = RemindObject(inter.user.id, task, remind_time, (True, None) if private else (
+            False, inter.channel.id), reminder_uuid)
+
+        async with self.bot.db_pool.acquire() as conn:
+            async with conn.transaction():
+                reminders = await conn.fetch("SELECT * FROM reminders WHERE id = $1", inter.user.id)
+                if len(reminders) >= 5:
+                    await inter.response.send_message("You have reached the maximum number of reminders (5).", ephemeral=True)
+                    return
+                await conn.execute("INSERT INTO reminders (id, task, remind_time, private, channel, reminder_id) VALUES ($1, $2, $3, $4, $5, $6)", inter.user.id, task, remind_obj.remind_time, private, str(remind_obj.channel[1]), str(uuid.uuid4()))
+                self.reminders[remind_obj.id] = remind_obj
 
         if not private:
-            await inter.response.send_message(f"I'll remind you here: `{remind_obj.text}`, <t:{int(remind_obj.time)}:R>")
+            await inter.response.send_message(f"I'll remind you here: `{remind_obj.task}`, <t:{int(remind_obj.remind_time)}:R>")
         else:
-            await inter.response.send_message(f"You'll receive a DM: `{remind_obj.text}`, <t:{int(remind_obj.time)}:R>", ephemeral=True)
-
-        self.reminders[remind_obj.id] = remind_obj
+            await inter.response.send_message(f"You'll receive a DM: `{remind_obj.task}`, <t:{int(remind_obj.remind_time)}:R>", ephemeral=True)
 
     @group.command(name="view", description="View existing reminders")
     async def view_reminders(self, inter: commands.Context):
-        if self.reminders:
-            reminder_list = "\n".join(f"- `{reminder.text}` | <t:{int(reminder.time)}:R>" for reminder in self.reminders.values())
-            await inter.response.send_message(f"Existing Reminders:\n{reminder_list}", ephemeral=True)
-        else:
-            await inter.response.send_message("There are no reminders set.", ephemeral=True)
+        async with self.bot.db_pool.acquire() as conn:
+            reminders = await conn.fetch("SELECT * FROM reminders WHERE id = $1", inter.user.id)
+            if reminders:
+                reminder_list = sorted(
+                    reminders, key=lambda r: r['remind_time'])
+                embed = discord.Embed(
+                    title="Existing Reminders")
+                for reminder in reminder_list:
+                    emoji = "🔒" if reminder['private'] else "#️⃣"
+                    task = reminder['task']
+                    channel = reminder['channel']
+                    embed.add_field(name=f"{emoji} {task}", value=f"Expires <t:{reminder['remind_time']}:R>\n{f'<#{channel}>' if channel.isdigit() else ''}", inline=False)
+                
+                embed.set_footer(text='🔒 DM reminders, #️⃣ Channel reminders')
+                await inter.response.send_message(embed=embed, ephemeral=True)
+            else:
+                await inter.response.send_message("There are no reminders set.", ephemeral=True)
 
 
-async def setup(ce):
-    await ce.add_cog(Reminder(ce))
+async def setup(bot):
+    await bot.add_cog(Reminder(bot))
