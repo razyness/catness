ff83106b08eb07e687356647b8146eaa4356fb41
razyness <razydiscord@protonmail.com>
2023-11-26 16:22:27 +0100

    try except for test guild, added a db check loop

--- playground.py
+++ playground.py
@@ -46,7 +46,8 @@ class Client(commands.AutoShardedBot):
 		if self.testing_guild_id:
 			guild = discord.Object(self.testing_guild_id)
 			self.tree.copy_global_to(guild=guild)
-			await self.tree.sync(guild=guild)
+			try: await self.tree.sync(guild=guild)
+			except: pass
 
 		await self.change_presence(
 			status=discord.Status.idle,
@@ -54,32 +55,6 @@ class Client(commands.AutoShardedBot):
 				type=discord.ActivityType.watching, name="loading up..."
 			),
 		)
-
-		async with self.db_pool.acquire() as conn:
-			db_guilds = await conn.fetch("SELECT id FROM servers")
-
-		db_guild_ids = {record['id'] for record in db_guilds}
-		fetched_guilds = {guild async for guild in self.fetch_guilds()}
-
-		for guild in fetched_guilds:
-			if guild.id not in db_guild_ids:
-				async with self.db_pool.acquire() as conn:
-					try:
-						await conn.execute("INSERT INTO servers (id) VALUES ($1)", guild.id)
-						print(f"➕ I was added to the guild {guild.name} | {guild.id}, and added it to my database")
-					except asyncpg.exceptions.PostgresError as e:
-						if "duplicate key value violates unique constraint" in str(e):
-							continue
-						print(f"🟧 I could not add the guild {guild.name} | {guild.id} to my database:", e)
-
-		for db_guild_id in db_guild_ids:
-			if db_guild_id not in [guild.id for guild in fetched_guilds]:
-				async with self.db_pool.acquire() as conn:
-					try:
-						await conn.execute("DELETE FROM servers WHERE id = $1", db_guild_id)
-						print(f"➖ I was removed from the guild with ID {db_guild_id}, and removed it from my database")
-					except asyncpg.exceptions.PostgresError as e:
-						print(f"🟧 I could not remove the guild with ID {db_guild_id} from my database:", e)
 				
 	async def on_ready(self):
 		if not hasattr(self, "uptime"):
